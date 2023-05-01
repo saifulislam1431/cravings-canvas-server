511a2efe77a12966a34e3bb6cf389051b1698edd
Saiful Islam <sakibbhaiya143@gmail.com>
2023-05-02 01:02:54 +0600

    API setup finished

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 5000;
 const cors = require('cors');
 const chefs = require("./data/chefData.json");
+const allRecipe = require("./data/allRecipe.json");
 
 app.use(cors());
 
@@ -12,6 +13,22 @@ app.get("/",(req,res)=>{
 
 app.get("/all-chef",(req,res)=>{
     res.send(chefs);
+})
+
+app.get("/all-recipe",(req,res)=>{
+    res.send(allRecipe);
+})
+
+app.get("/recipe-by-id/:id",(req,res)=>{
+    const id = req.params.id;
+    const filteredRecipes = allRecipe.filter(recipes => recipes.id == id);
+    res.send(filteredRecipes);
+})
+
+app.get("/recipe-by-chef-id/:id",(req,res)=>{
+    const id = req.params.id;
+    const filteredRecipes = allRecipe.filter(recipes => recipes.chefId == id);
+    res.send(filteredRecipes);
 })
 
 app.listen(port,()=>{
