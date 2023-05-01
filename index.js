25991f4535601a97f1743c977982f892b0c742c5
Saiful Islam <sakibbhaiya143@gmail.com>
2023-05-01 23:43:59 +0600

    All chef API loaded

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const cors = require('cors');
-
+const chefs = require("./data/chefData.json");
 
 app.use(cors());
 
 app.get("/",(req,res)=>{
     res.send("Cravings & Canvas server")
+})
+
+app.get("/all-chef",(req,res)=>{
+    res.send(chefs);
 })
 
 app.listen(port,()=>{
